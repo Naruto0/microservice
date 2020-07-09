c94650e0860fe5d58ad977d489e6c95ee194ef31
Filip Hron <filip.hron@gmail.com>
2020-07-09 22:49:40 +0200

    add heroku support

--- micro/__init__.py
+++ micro/__init__.py
@@ -3,8 +3,11 @@ from micro.settings import CONFIG
 
 app = Flask(__name__)
 
-# from micro.database import db
 from micro.models import *
 from micro.views import *
+
+from micro.database import init_db
+
+init_db()
 
 logging.basicConfig(filename=CONFIG.APP_LOG, level=logging.ERROR)
